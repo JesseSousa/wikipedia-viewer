9e337c10c16a38d3d112df6646ee3a4758bd293a
Jesse Sousa <rhadamesjesse14rj@gmail.com>
2020-08-03 20:07:42 -0300

    Updates

---
+++ precache-manifest.d3cfb1f08a58f538aab80aab9c806074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297641105654a76a8bd6e7e7d52a20df",
+    "url": "/wikipedia-viewer/index.html"
+  },
+  {
+    "revision": "cb647fb4577a6858edd7",
+    "url": "/wikipedia-viewer/static/css/main.ed7fdcae.chunk.css"
+  },
+  {
+    "revision": "71374b0b2eea7b24c3f1",
+    "url": "/wikipedia-viewer/static/js/2.dc3ab92b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wikipedia-viewer/static/js/2.dc3ab92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb647fb4577a6858edd7",
+    "url": "/wikipedia-viewer/static/js/main.876abbe9.chunk.js"
+  },
+  {
+    "revision": "3fe2182689b3a7b52785",
+    "url": "/wikipedia-viewer/static/js/runtime-main.ea7cd611.js"
+  }
+]);
